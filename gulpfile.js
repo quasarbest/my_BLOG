bd6ae47deaa734d28d17aa0c8ed51aba8634dfa9
quasarbest <sunny_child88@mail.ru>
2021-06-07 01:24:02 +0600

    work

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ let path = {
     },
     src: {
         html: [sourse_folder + '/*.html', '!' + sourse_folder + '/_*.html'],
-        css: sourse_folder+ '/style/style.sass',
+        css: sourse_folder+ '/style/style.css',
         js: sourse_folder + '/js/script.js',
         img: sourse_folder + '/image/**/*.{jpg,png,svg,giv,ico,webp}',
         fonts: sourse_folder + '/fonts/*.ttf',
@@ -29,10 +29,12 @@ let path = {
 }
 
 let { src, dest } = require('gulp'),
-    
     gulp = require('gulp'),
     browsersync = require('browser-sync').create(),
-    fileinclude = require('gulp-file-include');
+    fileinclude = require('gulp-file-include'),
+    del = require('del'),
+    sass = require('gulp-sass');
+
     
 
 function browserSync(params) {
@@ -49,17 +51,33 @@ function html() {
     return src(path.src.html)
         .pipe(fileinclude())
         .pipe(dest(path.build.html))
-    .pipe(browsersync.stream())
+        .pipe(browsersync.stream())
+}
+
+function css() {
+    return src(path.src.css)
+        .pipe(
+            sass({
+               outputStyle: 'expanded' 
+            })
+        )
+        .pipe(dest(path.build.css))
+        .pipe(browsersync.stream())  
 }
 
 function watchFiles(params) {
     gulp.watch([path.watch.html], html);
+    gulp.watch([path.watch.css], css);
+    
+}
+function clean(params) {
+    return del(path.clean);
 }
 
-
-let build = gulp.series(html);
+let build = gulp.series(clean, gulp.parallel(css, html));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
